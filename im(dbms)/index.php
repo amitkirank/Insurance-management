531278fac41dbfb016f668310bbd9503873995d9
amitkirank <57851156+amitkirank@users.noreply.github.com>
2020-08-04 14:25:08 +0530

    Add files via upload

---
+++ im(dbms)/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>
+Life Insurance 
+</title>
+</head>
+<body>
+
+<link href = "policy/registration.css" type = "text/css" rel = "stylesheet" />  
+<link href = "style.css" type = "text/css" rel = "stylesheet" />  
+
+<nav> 
+
+<ul>
+	<li><a href="agent/agent.php"> <h3>Agent Registration</h3></a></li>
+	<li><a href="client/client.php"><h3> Client Registration</h3></a></li>
+	<li><a href="policy/policy.php"><h3> Policy Registration</h3></a></li>
+	<li><a href="premium/premium.php"><h3> Premium Registration</h3></a></li>
+	<li><a href="agent/modified1.php"><h3> Agents Data</h3></a></li>
+	
+</ul>
+
+</nav> 
+<div class="title">
+<h1><center>Life Insurance Corporation of NMIT</center></h1>
+<h5 align="center">DONE BY</h5>
+<h2>AMIT BHAT (1NT17IS018) </h2>
+<h2>AMIT KIRAN(1NT17IS019)</h2>
+<h2>DHANUSH M (1NT17IS050)</h2>
+</di	
+v>
+<div class="links">
+<div class="subtitle">
+	<marquee behavior="scroll" direction="right">
+
+
+ <p class="text-muted text-center">
+ <small>Copyright © NITTE(AMIT,DHANUSH,AMIT K)</small></p>
+</marquee>
+</div>
+
+<ul style="position: fixed; bottom: 0; width: 100%;">
+	<li><a href="client/modified1.php"><h3> Customers Data</h3></a></li>
+	<li><a href="policy/modified1.php"><h3> Policies Data</h3></a></li>
+	<li><a href="premium/modified1.php"><h3> Premiums Data</h3></a></li>
+</ul>
+
+</div>
